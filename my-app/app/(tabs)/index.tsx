934fc0fc24205e948d62d4336c4d6c857173ee7a
Shockz132 <jethrochia1@gmail.com>
2025-04-17 00:08:40 +0800

    Layout changes
    added Tabs and Tabs.Screen for navigation and routing

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { Text, View } from "react-native";
 import { Link } from 'expo-router'
-import './globals.css'
+import '../globals.css'
 
 export default function Index() {
 
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary">kill me now!</Text>
-      <Link href='/test' className="underline text-blue-500">commit suicide!</Link>
+      <Text>
+        <Text className="text-5xl text-primary">kill me now</Text>
+        <Text className="text-5xl text-red-500">!</Text>
+      </Text>
       <Link href='/testParams/yourmom' className="underline text-blue-500">your mom!</Link>
     </View>
   );
